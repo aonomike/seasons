c014bf35612f363ee75be776c7aa9edd952890f0
aonomike <aonomike@gmail.com>
2018-12-10 15:17:35 +0300

    Add state for geolocation latitude

--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
 
+    constructor(props){
+        super(props);
+        //This is the ONLY time we do direct assignment to the state
+        this.state = {latitude: null, errorMessage:''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called set state!!!
+                this.setState({latitude: position.coords.latitude})
+            },
+            err => this.setState({errMessage: err.message}) 
+        );
+    }
+    // react says we have to define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
             err => console.log(err) 
         );
-        return (
-            <div>
-                Latitude:
-            </div>)
+       
+        if (this.state.errorMessage && !this.state.latitude){
+            return <div>{this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <div>{this.state.latitude}</div>
+        }
+        return <div>Loading!</div>
+            
         };
 }
 
